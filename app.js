c4a0ad8cb3f5ef063c2d7dca794e58da79383c89
oliyam <apfel500baum@gmail.com>
2024-09-28 22:12:36 +0200

    port

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
+const port = 3030;
+
 app.use(express.static('bin'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname+"/bin/res/index.html")
 })
 
-app.listen(3000, () => {
-  console.log("Server is Running")
+app.listen(port, () => {
+  console.log("==================")
+  console.log("==================")
+  console.log("CLASSIS-server is running:")
+  console.log("PORT:"+port)
 })
