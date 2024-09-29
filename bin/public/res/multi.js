59597ab5652672a080c4e254e04a9c72e9fa6e48
oliyam <apfel500baum@gmail.com>
2024-09-29 22:42:43 +0200

    update multi.js

--- bin/public/res/multi.js
+++ bin/public/res/multi.js
@@ -3,7 +3,7 @@ class multiplayer {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      return xhttp.responseText;
+      return JSON.parse(xhttp.responseText);
    }
   };
   xhttp.open("GET","battle", true);
