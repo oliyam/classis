0b4120d17db180bef8d4af76e992e36217459664
oliyam <apfel500baum@gmail.com>
2024-09-30 22:59:50 +0200

    update multi.js

--- bin/public/res/multi.js
+++ bin/public/res/multi.js
@@ -1,26 +1,19 @@
 class multiplayer {
   
-  xhttp;
-  url;
-  
-  constructor(url){
-    this.url=url||'/';
-    this.xhttp = new XMLHttpRequest();
-  }
-  
   get_game = new Promise((res) =>{
- 
-    this.xhttp.onreadystatechange = function() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        res(JSON.parse(this.xhttp.responseText))
+        res(JSON.parse(xhttp.responseText))
      }
     };
-    this.xhttp.open("GET",this.url+"battle_rx", true);
-    this.xhttp.send();
+    xhttp.open("GET", "battle_rx", true);
+    xhttp.send();
   })
   
   send_game = (data) => {
-    this.xhttp.open("POST", this.url+"battle_tx");
-    this.xhttp.send(JSON.stringify(data));
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "battle_tx");
+    xhttp.send(JSON.stringify(data));
   }
 }
