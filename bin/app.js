df7438049d95a2ee3b3cb71014a4e89809a10641
oliyam <apfel500baum@gmail.com>
2024-10-01 08:25:15 +0200

    update app.js

--- bin/app.js
+++ bin/app.js
@@ -31,7 +31,7 @@ app.get('/battle_rx', (req, res) => {
 })
 
 app.post('/battle_tx', (req, res) => {
-  console.log(req.body)
+  console.log(req)
   game=Object.assign(game, req.body)
   res.sendStatus(200)
 })
