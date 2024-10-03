46c71acc2becb0be0acd692f81f509736563418c
oliyam <apfel500baum@gmail.com>
2024-10-03 19:56:36 +0200

    update server-model.js

--- bin/server-model.js
+++ bin/server-model.js
@@ -32,7 +32,7 @@ exports.run = () => {
     }
     
     req_game(data){
-      this.server_game.scan(data.f)
+      //this.server_game.scan(data.f)
       var cg = Object.assign({}, this.server_game);
 
       cg.vessels.forEach(v => {
