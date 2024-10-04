4438b7455016a96446ef5c259d3ebd992c106d70
oliyam <apfel500baum@gmail.com>
2024-10-04 16:25:17 +0200

    update server-model.js

--- bin/server-model.js
+++ bin/server-model.js
@@ -54,11 +54,10 @@ exports.run = () => {
       splashes = [];
       
       constructor(faction){
-        for (var i = 0; i < 4; i++){
-          
+        for (var i = 0; i < 4; i++)
           this.vessels.push(new vessel(i, 'redpp', "ROCINANTE-0"+i, [{x:50*i+50,y:100}], 100, 50, null, new radar(100)))
+        for (var i = 0; i < 4; i++)
           this.vessels.push(new vessel(i, 'blue', "ENTERPRISE-0"+i, [{ x: 50*i+50, y: 500 }], 100, 50, new weapon(69, 200), new radar(200, 1)))
-        }
       }
       
       scan(faction){
