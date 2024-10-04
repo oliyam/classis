18a2ca7b2eae75b7a9234914762cdc409f83f242
oliyam <apfel500baum@gmail.com>
2024-10-04 19:19:55 +0200

    update client.js

--- bin/public/client.js
+++ bin/public/client.js
@@ -30,13 +30,13 @@ window.onload = ()=> {
 
   document.getElementById('turn').onclick=()=>{
     disableUI(true)
-       document.getElementById('req').disabled=true;
+       document.getElementById('req').disabled=fase;
     view.draw_game(battle,1)
     mult.send_game(battle).then( res => {
       if (res.turned){
         battle.turn()
         view.draw_game(battle,1)
-        document.getElementById('req').disabled=false;
+       // document.getElementById('req').disabled=false;
       }
       else{
         view.draw_game(battle,0)
