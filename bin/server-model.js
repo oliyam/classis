daee4285e36aaed20c5d96c94c0946a5755a0245
oliyam <apfel500baum@gmail.com>
2024-10-04 16:32:07 +0200

    update server-model.js

--- bin/server-model.js
+++ bin/server-model.js
@@ -33,8 +33,9 @@ exports.run = () => {
     req_game(data){
       this.server_game.scan(data.f)
       var cg = JSON.parse(JSON.stringify(this.server_game))
-
+      console.log(cg.vessels.length)
       cg.vessels.forEach(v => {
+        console.log('.')
         console.log(data.f+" !== "+v.faction+" = "+ (v.faction!==data.f) )
         if (v.faction!==data.f) 
           cg.vessels.splice(cg.vessels.indexOf(v), 1);
