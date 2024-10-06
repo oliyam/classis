8da423bccbd30b4179c12644effcbdf19184d92b
oliyam <apfel500baum@gmail.com>
2024-10-06 15:55:01 +0200

    update view.js

--- bin/public/res/view.js
+++ bin/public/res/view.js
@@ -338,7 +338,7 @@
             this.ctx.fillText(txt, x, y + px *  line++);
            if (msg) {
             this.ctx.fillStyle = "red"
-            this.ctx.clearRect(x, y + px * (line - 1 + 0==this.msg_line%4?this.msg_line%4-1:0), msg_l, 0==this.msg_line%4?px*4:px);
+            this.ctx.clearRect(x, y + px * (line - 1 + 0==this.msg_line%4?this.msg_line%4-1:1), msg_l, 0==this.msg_line%4?px*4:px);
             this.ctx.fillText(msg, x, y + px * (line + this.msg_line++%4));
             msg=null;
            }
