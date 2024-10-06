b3c314b33d277d40cb70ddf158d1f18af8500988
oliyam <apfel500baum@gmail.com>
2024-10-06 12:50:02 +0200

    update

--- bin/public/res/view.js
+++ bin/public/res/view.js
@@ -291,14 +291,14 @@
     this.ctx.clearRect(0, 0, this.size.x, this.size.y);
     if (inactive)
       this.ctx.globalAlpha = .25;
-      
-    this.draw_radars(game.vessels)
-    this.draw_splashes(game)
-    game.vessels.forEach(v => {
-        this.draw_vessel(v, game.selected_v == v.id);
-    });
+    if(!msg){
+      this.draw_radars(game.vessels)
+      this.draw_splashes(game)
+      game.vessels.forEach(v => {
+          this.draw_vessel(v, game.selected_v == v.id);
+      });
+    }
     this.ctx.globalAlpha=1;
-  
     if (inactive) {
       var 
         x=20,
