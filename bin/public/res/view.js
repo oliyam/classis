9f8cdeb4b35d1a90917340b41f3d5ef83dfb36da
oliyam <apfel500baum@gmail.com>
2024-10-06 10:54:37 +0200

    update view.js

--- bin/public/res/view.js
+++ bin/public/res/view.js
@@ -295,7 +295,7 @@
     this.draw_radars(game.vessels)
     this.draw_splashes(game)
     game.vessels.forEach(v => {
-        this.draw_vessel(v, game.selected_v == v.id && !this.loading);
+        this.draw_vessel(v, game.selected_v == v.id);
     });
     this.ctx.globalAlpha=1;
   
